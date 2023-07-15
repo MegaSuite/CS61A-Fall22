0ad0e3b1a0f5cade6a57ad3c3f74323ffc2d97a3
Konrad Locas <flansmaster@163.com>
2023-07-15 17:00:42 +0800

    lab09 lab10 hw07 done

--- Labs/lab10/lab10.scm
+++ Labs/lab10/lab10.scm
@@ -1,9 +1,14 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2) (if (< num1 num2) -1 (if (> num1 num2) 1 0)))
+(define (over-or-under num1 num2) 
+    (cond
+        ((> num1 num2) 1)
+        ((< num1 num2) -1)
+        (else 0))
+    )
+(define (make-adder num) (lambda (inc) (+ inc num)))
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+(define (composed f g) (lambda (x) (f (g x))))
 
-(define (composed f g) 'YOUR-CODE-HERE)
+(define lst (cons '(1) (cons 2 (cons (list 3 4) (cons 5 nil)))))
 
-(define lst 'YOUR-CODE-HERE)
-
-(define (duplicate lst) 'YOUR-CODE-HERE)
+(define (duplicate lst) (if (= (length lst) 0) nil (cons (car lst) (cons (car lst) (duplicate (cdr lst))))))
